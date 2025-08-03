a7cefec403218a44596b5e8978f8d21bb20649f7
chethankumarhl <22btrcn062@jainuniversity.ac.in>
2025-08-03 20:21:41 +0530

    fixing cors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -84,8 +84,8 @@ export const login = asyncHandler(async (req, res) => {
   }
    res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Lax', // or 'None' if cross-origin and HTTPS
+      secure: true,
+      sameSite: None, // or 'None' if cross-origin and HTTPS
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
     
